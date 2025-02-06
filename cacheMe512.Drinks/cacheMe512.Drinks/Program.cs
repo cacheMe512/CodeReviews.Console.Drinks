d57fe078c3656e87dd7f50f03a2bb3133a0dd0be
Kornel Filipiak <kornelf048@gmail.com>
2025-02-06 16:45:39 -0500

    UserInput + validation

--- cacheMe512.Drinks/cacheMe512.Drinks/Program.cs
+++ cacheMe512.Drinks/cacheMe512.Drinks/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using cacheMe512.Drinks;
+
+UserInput userInput = new();
+userInput.GetCategoriesInput();
